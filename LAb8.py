27cb56273feabb61eb4a8d5b5c2feface10ba5a4
Masum Billah <95751615+masum200119@users.noreply.github.com>
2024-06-03 00:03:32 +0600

    Update LAb8.py

--- LAb8.py
+++ LAb8.py
@@ -2,45 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import freqz
 
-# Given specifications
-fs = 20000  # Sampling frequency in Hz
-passband_edge = 2000  # Passband edge in Hz
-stopband_edge = 5000  # Stopband edge in Hz
-filter_length = 21  # Length of the filter
+# Specifications
+Fs = 20000  # Sampling frequency in Hz
+F_pass = 2000  # Passband edge in Hz
+F_stop = 5000  # Stopband edge in Hz
+N = 21  # Filter length
 
-# Normalized frequencies
-passband_edge_normalized = passband_edge / (fs / 2)
-stopband_edge_normalized = stopband_edge / (fs / 2)
+# Calculate the normalized cutoff frequency
+F_cutoff = (F_pass + F_stop) / 2
+normalized_cutoff = F_cutoff / (Fs / 2)
 
-# Ideal impulse response of the low-pass filter
-def ideal_lp(cutoff_freq, num_taps):
-    M = num_taps - 1
-    n = np.arange(0, num_taps)
-    h = np.sinc(2 * cutoff_freq * (n - M / 2))
-    return h
+# Generate the ideal lowpass filter (sinc function)
+n = np.arange(N)
+h_ideal = np.sinc(2 * normalized_cutoff * (n - (N - 1) / 2))
 
-# Design the band-pass filter
-h_low = ideal_lp(stopband_edge_normalized, filter_length)
-h_high = ideal_lp(passband_edge_normalized, filter_length)
-h_bandpass = h_low - h_high
-
-# Apply Hanning window
-window = np.hanning(filter_length)
-h_bandpass_windowed = h_bandpass * window
+# Apply the Hanning window
+hanning_window = np.hanning(N)
+h = h_ideal * hanning_window
 
 # Normalize the filter coefficients
-h_bandpass_windowed /= np.sum(h_bandpass_windowed)
+h = h / np.sum(h)
 
-# Frequency response of the filter
-w, h = freqz(h_bandpass_windowed, worN=8000)
+# Frequency response
+w, H = freqz(h, worN=8000)
+frequencies = w * Fs / (2 * np.pi)
 
-# Plot the magnitude response
-plt.figure(figsize=(14, 7))
-plt.plot(0.5 * fs * w / np.pi, np.abs(h), 'b')
-plt.title('Bandpass Filter Frequency Response')
+# Plot the impulse response
+plt.figure(figsize=(12, 6))
+
+plt.subplot(2, 1, 1)
+plt.stem(n, h)
+plt.title('Impulse Response')
+plt.xlabel('Samples')
+plt.ylabel('Amplitude')
+
+# Plot the frequency response
+plt.subplot(2, 1, 2)
+plt.plot(frequencies, 20 * np.log10(abs(H)))
+plt.title('Frequency Response')
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Gain')
+plt.ylabel('Magnitude (dB)')
 plt.grid()
-plt.axvline(passband_edge, color='green')  # Passband edge
-plt.axvline(stopband_edge, color='red')  # Stopband edge
+plt.ylim([-100, 5])
+
+plt.tight_layout()
 plt.show()
